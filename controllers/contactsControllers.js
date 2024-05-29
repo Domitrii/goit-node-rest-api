952acdf82e3ecc19b6e20d5474e6f2d7e3ae9d2d
Domitrii <126395589+Domitrii@users.noreply.github.com>
2024-05-29 17:06:19 +0200

    correct version

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,88 +1,97 @@
 import contactsService from "../services/contactsServices.js";
+import { createContactSchema, updateContactSchema } from "../schemas/contactsSchemas.js";
 import * as fs from "node:fs/promises";
 import path from "node:path";
 
 const contactsPath = path.resolve("db", "contacts.json");
 
 export const getAllContacts = async (req, res) => {
-    try {
-        const contacts = await contactsService.listContacts();
-        res.status(200).json(contacts);
-    } catch (error) {
-        console.error('Error is: ', error);
-        res.status(500).json({ message: 'Failed to get contacts' });
-    }
+  try {
+    const contacts = await contactsService.listContacts();
+    res.status(200).json(contacts);
+  } catch (error) {
+    console.error('Error is: ', error);
+    res.status(500).json({ message: 'Failed to get contacts' });
+  }
 };
 
 export const getOneContact = async (req, res) => {
-    try {
-        const contactId = req.params.id;
-        const contact = await contactsService.getContactById(contactId);
-        if (contact.message === "not found") {
-            res.status(404).json({ message: 'Contact not found' });
-        } else {
-            res.status(200).json(contact);
-        }
-    } catch (error) {
-        console.error('Error is: ', error);
-        res.status(500).json({ message: 'Failed to get contact' });
+  try {
+    const contactId = req.params.id;
+    const contact = await contactsService.getContactById(contactId);
+    if (contact.message === "not found") {
+      res.status(404).json({ message: 'Contact not found' });
+    } else {
+      res.status(200).json(contact);
     }
+  } catch (error) {
+    console.error('Error is: ', error);
+    res.status(500).json({ message: 'Failed to get contact' });
+  }
 };
 
 export const deleteContact = async (req, res) => {
-    try {
-        const contactId = req.params.id;
-        const deletedContact = await contactsService.removeContact(contactId);
-        if (!deletedContact) {
-            res.status(404).json({ message: 'Contact not found' });
-        } else {
-            res.status(200).json(deletedContact);
-        }
-    } catch (error) {
-        console.error('Error is: ', error);
-        res.status(500).json({ message: 'Failed to delete contact' });
+  try {
+    const contactId = req.params.id;
+    const deletedContact = await contactsService.removeContact(contactId);
+    if (!deletedContact) {
+      res.status(404).json({ message: 'Contact not found' });
+    } else {
+      res.status(200).json(deletedContact);
     }
+  } catch (error) {
+    console.error('Error is: ', error);
+    res.status(500).json({ message: 'Failed to delete contact' });
+  }
 };
 
 export const createContact = async (req, res) => {
-    try {
-        const { name, email, phone } = req.body;
-        const newContact = await contactsService.addContact(name, email, phone);
-        res.status(201).json(newContact);
-    } catch (error) {
-        console.error('Error is: ', error);
-        res.status(500).json({ message: 'Failed to create contact' });
+  try {
+    const { error, value } = createContactSchema.validate(req.body);
+    if (error) {
+      return res.status(400).json({ message: error.message });
     }
+    const { name, email, phone } = value;
+    const newContact = await contactsService.addContact(name, email, phone);
+    res.status(201).json(newContact);
+  } catch (error) {
+    console.error('Error is: ', error);
+    res.status(500).json({ message: 'Failed to create contact' });
+  }
 };
 
 export const updateContact = async (req, res) => {
-    try {
-        const contactId = req.params.id;
-        const { name, email, phone } = req.body;
-        const contacts = await contactsService.listContacts();
-        const contactIndex = contacts.findIndex(({ id }) => id === contactId);
-
-        if (contactIndex === -1) {
-            res.status(404).json({ message: 'Contact not found' });
-            return;
-        }
-
-        const updatedContact = {
-            ...contacts[contactIndex],
-            name: name || contacts[contactIndex].name,
-            email: email || contacts[contactIndex].email,
-            phone: phone || contacts[contactIndex].phone,
-        };
-
-        contacts[contactIndex] = updatedContact;
-        await fs.writeFile(
-            contactsPath,
-            JSON.stringify(contacts, undefined, 2)
-        );
-
-        res.status(200).json(updatedContact);
-    } catch (error) {
-        console.error('Error is: ', error);
-        res.status(500).json({ message: 'Failed to update contact' });
+  try {
+    const contactId = req.params.id;
+    const { error, value } = updateContactSchema.validate(req.body);
+    if (error) {
+      return res.status(400).json({ message: error.message });
     }
+    const { name, email, phone } = value;
+    const contacts = await contactsService.listContacts();
+    const contactIndex = contacts.findIndex(({ id }) => id === contactId);
+
+    if (contactIndex === -1) {
+      res.status(404).json({ message: 'Contact not found' });
+      return;
+    }
+
+    const updatedContact = {
+      ...contacts[contactIndex],
+      name: name || contacts[contactIndex].name,
+      email: email || contacts[contactIndex].email,
+      phone: phone || contacts[contactIndex].phone,
+    };
+
+    contacts[contactIndex] = updatedContact;
+    await fs.writeFile(
+      contactsPath,
+      JSON.stringify(contacts, undefined, 2)
+    );
+
+    res.status(200).json(updatedContact);
+  } catch (error) {
+    console.error('Error is: ', error);
+    res.status(500).json({ message: 'Failed to update contact' });
+  }
 };
--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -1,9 +1,30 @@
 import Joi from "joi";
 
 export const createContactSchema = Joi.object({
-
-})
+  name: Joi.string().min(3).max(30).required().messages({
+    'string.empty': 'Name is required',
+    'string.min': 'Name should have a minimum length of 3 characters',
+    'string.max': 'Name should have a maximum length of 30 characters',
+  }),
+  email: Joi.string().email().required().messages({
+    'string.empty': 'Email is required',
+    'string.email': 'Email must be a valid email address',
+  }),
+  phone: Joi.string().pattern(/^\d+$/).required().messages({
+    'string.empty': 'Phone is required',
+    'string.pattern.base': 'Phone must be a valid phone number',
+  }),
+});
 
 export const updateContactSchema = Joi.object({
-
-})
+  name: Joi.string().min(3).max(30).optional().messages({
+    'string.min': 'Name should have a minimum length of 3 characters',
+    'string.max': 'Name should have a maximum length of 30 characters',
+  }),
+  email: Joi.string().email().optional().messages({
+    'string.email': 'Email must be a valid email address',
+  }),
+  phone: Joi.string().pattern(/^\d+$/).optional().messages({
+    'string.pattern.base': 'Phone must be a valid phone number',
+  }),
+}).or('name', 'email', 'phone'); // At least one field should be present for update
